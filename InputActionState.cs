58975b2014d98bc52399f8ff6a8f0613f426bdc7
PhilippFors <57153492+PhilippFors@users.noreply.github.com>
2023-06-11 17:05:12 +0200

    Update InputActionState.cs

--- InputActionState.cs
+++ InputActionState.cs
@@ -1,64 +1,61 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Player.Input.Data
+public class InputActionState
 {
-    public class InputActionState
+    public bool Triggered => action.triggered;
+    public bool Released => action.WasReleasedThisFrame();
+    public bool IsPressed { get; private set; }
+    public float PerformedTime { get; private set; }
+    public float HoldTime
     {
-        public bool Triggered => action.triggered;
-        public bool Released => action.WasReleasedThisFrame();
-        public bool IsPressed { get; private set; }
-        public float PerformedTime { get; private set; }
-        public float HoldTime
+        get
         {
-            get
+            if (!IsPressed && !Released)
             {
-                if (!IsPressed && !Released)
-                {
-                    return 0;
-                }
-
-                return Time.time - PerformedTime;
+                return 0;
             }
+
+            return Time.time - PerformedTime;
         }
-
-        private readonly InputAction action;
-
-        public InputActionState(InputAction action)
-        {
-            this.action = action;
-            action.started += OnTriggered;
-            action.canceled += OnTriggered;
-        }
-
-        private void OnTriggered(InputAction.CallbackContext context)
-        {
-            if (context.started)
-            {
-                IsPressed = true;
-                PerformedTime = Time.time;
-            }
-            else if (context.canceled)
-            {
-                IsPressed = false;
-            }
-        }
-
-        public T ReadValue<T>() where T : struct
-        {
-            return action.ReadValue<T>();
-        }
-
-        public void Enable()
-        {
-            action.Enable();
-        }
-
-        public void Disable()
-        {
-            action.Disable();
-        }
-
-        public InputAction GetAction() => action;
     }
+
+    private readonly InputAction action;
+
+    public InputActionState(InputAction action)
+    {
+        this.action = action;
+        action.started += OnTriggered;
+        action.canceled += OnTriggered;
+    }
+
+    private void OnTriggered(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsPressed = true;
+            PerformedTime = Time.time;
+        }
+        else if (context.canceled)
+        {
+            IsPressed = false;
+        }
+    }
+
+    public T ReadValue<T>() where T : struct
+    {
+        return action.ReadValue<T>();
+    }
+
+    public void Enable()
+    {
+        action.Enable();
+    }
+
+    public void Disable()
+    {
+        action.Disable();
+    }
+
+    public InputAction GetAction() => action;
 }
